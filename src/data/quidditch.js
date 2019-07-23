ec2f041dfaad0e59529c1dab7b52fb0fcd4e37e7
Alex Spencer <alexander.j.spencer@gmail.com>
2019-07-23 10:14:28 -0700

    Fixed pictures edited CSS and layout

--- src/data/quidditch.js
+++ src/data/quidditch.js
@@ -1,21 +1,31 @@
+const comet260 = {
+    code: 'comet-260',
+    name: 'Comet 260',
+    image: 'assets/comet-260.png',
+    description: 'A beginner broom, ready for any witch or wizard!',
+    category: 'broomstick',
+    price: 2000.00,
+    cost: 200.00
+};
+
 const nimbus2000 = {
     code: 'nimbus-2000',
     name: 'Nimbus 2000',
     image: 'assets/nimbus-2000.png',
     description: 'A speedy, nimble, top-of-the-line broomstick',
     category: 'broomstick',
-    price: 5000,
-    cost: 500 
+    price: 5000.00,
+    cost: 500.00
 };
 
 const nimbus2001 = {
     code: 'nimbus-2001',
     name: 'Nimbus 2001',
-    image: 'assets/numbus-2001.jpg',
+    image: 'assets/nimbus-2001.png',
     description: 'A minor upgrade to the Nimbus 2000, enhancements to top speed!',
     category: 'broomstick',
-    price: 6500,
-    cost: 1500
+    price: 6500.00,
+    cost: 1500.00
 };
 
 const firebolt = {
@@ -24,8 +34,8 @@ const firebolt = {
     image: 'assets/firebolt.png',
     description: 'The true purebred of quidditch broomsticks, reserved for the best flyers',
     category: 'broomstick',
-    price: 10000,
-    cost: 2500
+    price: 10000.00,
+    cost: 2500.00
 };
 
 const goldenSnitch = {
@@ -34,8 +44,8 @@ const goldenSnitch = {
     image: 'assets/golden-snitch.png',
     description: 'Unthinkably small, and wicked fast, good luck catching this one!',
     category: 'quidditch-tools',
-    price: 2000,
-    cost: 700
+    price: 2000.00,
+    cost: 700.00
 };
 
 const quidditchStarterSet = {
@@ -44,11 +54,12 @@ const quidditchStarterSet = {
     image: 'assets/quidditch-set.png',
     description: 'A perfect starter set for any hobbyist witch or wizard to get into quidditch!',
     category: 'quidditch-tools',
-    price: 3500,
-    cost: 900
+    price: 3500.00,
+    cost: 900.00
 };
 
 const quidditchProducts = [
+    comet260,
     nimbus2000,
     nimbus2001,
     firebolt,
