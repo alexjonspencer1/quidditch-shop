7021f46482d31c41a2b6b844ba87cdda29d8d5ab
Alex Spencer <alexander.j.spencer@gmail.com>
2019-07-22 21:06:56 -0700

    Improved CSS HTML layout and some object literals in JS

---
+++ src/data/quidditch.js
@@ -0,0 +1,59 @@
+const nimbus2000 = {
+    code: 'nimbus-2000',
+    name: 'Nimbus 2000',
+    image: 'assets/nimbus-2000.png',
+    description: 'A speedy, nimble, top-of-the-line broomstick',
+    category: 'broomstick',
+    price: 5000,
+    cost: 500 
+};
+
+const nimbus2001 = {
+    code: 'nimbus-2001',
+    name: 'Nimbus 2001',
+    image: 'assets/numbus-2001.jpg',
+    description: 'A minor upgrade to the Nimbus 2000, enhancements to top speed!',
+    category: 'broomstick',
+    price: 6500,
+    cost: 1500
+};
+
+const firebolt = {
+    code: 'firebold',
+    name: 'Firebolt',
+    image: 'assets/firebolt.png',
+    description: 'The true purebred of quidditch broomsticks, reserved for the best flyers',
+    category: 'broomstick',
+    price: 10000,
+    cost: 2500
+
+};
+
+const goldenSnitch = {
+    code: 'golden-snitch',
+    name: 'Golden Snitch',
+    image: 'assets/golden-snitch.png',
+    description: 'Unthinkably small, and wicked fast, good luck catching this one!',
+    category: 'quidditch-tools',
+    price: 2000,
+    cost: 700
+};
+const quidditchStarterSet = {
+    code: 'quidditch-starter-set',
+    name: 'Quidditch Starter Set',
+    image: 'assets/quidditch-set.png',
+    description: 'A perfect starter set for any hobbyist witch or wizard to get into quidditch!',
+    category: 'quidditch-tools',
+    price: 3500,
+    cost: 900
+};
+
+const quidditchProducts = [
+    nimbus2000,
+    nimbus2001,
+    firebolt,
+    goldenSnitch,
+    quidditchStarterSet,
+];
+
+export default quidditchProducts;
