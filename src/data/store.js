b7c3997ac665eb01380681bcfd4c459c4d75ac30
Alex Spencer <alexander.j.spencer@gmail.com>
2019-07-24 20:39:49 -0700

    Worked a bit more on last function test

--- src/data/store.js
+++ src/data/store.js
@@ -1,4 +1,5 @@
 import quidditchProducts from './quidditch.js';
+import { findProduct } from '../register.js';
 
 const PRODUCT_KEY = 'products';
 const SHOPPING_CART_KEY = 'shopping-cart';
@@ -32,6 +33,27 @@ const store = {
         }
         return shoppingCart;
     },
+    orderProduct(code) {
+
+        // get shopping cart 
+
+        const shoppingCart = store.getShoppingCart();
+
+        // get code for product 
+
+        const productFromCode = findProduct(quidditchProducts, code);
+        console.log(productFromCode);
+
+        // push code into shopping cart array 
+        const lineItem = {
+            code: 'nimbus-2000',
+            quantity: 1
+        };
+
+        shoppingCart.push(lineItem);
+        
+        // save shopping cart back to storage
+    }
 
 };
 
