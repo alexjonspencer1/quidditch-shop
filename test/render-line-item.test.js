6ce305934d5a5b7acd83ac59b89e0922c7d8877a
Alex Spencer <alexander.j.spencer@gmail.com>
2019-07-26 12:09:10 -0700

    Updated broken test cause I changed things

--- test/render-line-item.test.js
+++ test/render-line-item.test.js
@@ -14,7 +14,7 @@ test('renders a line item', assert => {
     };
 
     const nimbus2000 = findProduct(quidditchProducts, lineItem.code);
-    const expected = '<tr><td class="text-align-left">2</td><td class="text-align-left">Nimbus 2000</td><td class="text-align-right">$5,000.00</td><td class="text-align-right">$10,000.00</td></tr>';
+    const expected = '<tr><td class="text-align-left">Nimbus 2000</td><td class="text-align-right">2</td><td class="text-align-right">$5,000.00</td><td class="text-align-right">$10,000.00</td></tr>';
     
     const dom = renderLineItem(lineItem, nimbus2000);
     const html = dom.outerHTML;
